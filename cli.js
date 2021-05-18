7b7e2d65ecc29bfc2ad2b337a730e5363afef0c6
kainlite <kainlite@gmail.com>
2021-05-18 12:22:27 -0300

    Specify memory limit

--- cli.js
+++ cli.js
@@ -1,13 +1,22 @@
 #!/usr/bin/env node
-'use strict';
-const meow = require('meow');
-const killFirefox = require('.');
+'use strict'
+const meow = require('meow')
+const killFirefox = require('.')
 
-const cli = meow(`
+const cli = meow(
+  `
   Usage
     $ kill-firefox-tabs
-`, {
-  flags: null
-});
+`,
+  {
+    flags: {
+      memory: {
+        type: 'number',
+        default: 200,
+        alias: 'm',
+      },
+    },
+  }
+)
 
-killFirefox(cli.flags);
+killFirefox(cli.flags)
