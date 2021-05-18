c4202f1f4c69f5121c949e875e0921312fde32cd
kainlite <kainlite@gmail.com>
2021-05-18 12:31:26 -0300

    Add usage

--- cli.js
+++ cli.js
@@ -1,22 +1,24 @@
 #!/usr/bin/env node
-'use strict'
-const meow = require('meow')
-const killFirefox = require('.')
+"use strict";
+const meow = require("meow");
+const killFirefox = require(".");
 
 const cli = meow(
   `
   Usage
     $ kill-firefox-tabs
+
+    $ kill-firefox-tabs -m 250
 `,
   {
     flags: {
       memory: {
-        type: 'number',
+        type: "number",
         default: 200,
-        alias: 'm',
+        alias: "m",
       },
     },
   }
-)
+);
 
-killFirefox(cli.flags)
+killFirefox(cli.flags);
--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-'use strict'
-const fkill = require('fkill')
-const psList = require('ps-list')
+"use strict";
+const fkill = require("fkill");
+const psList = require("ps-list");
 
 module.exports = async (options = {}) => {
-  const list = await psList()
+  const list = await psList();
 
   const processes = {
-    firefox: process.platform === 'darwin' ? 'Firefox' : 'firefox',
-  }
+    firefox: process.platform === "darwin" ? "Firefox" : "firefox",
+  };
 
   const kill_limit =
-    parseInt(options.memory) > 50 ? parseInt(options.memory) / 100 : 0.2
+    parseInt(options.memory) > 50 ? parseInt(options.memory) / 100 : 0.2;
 
   const pids = list
     .filter(
@@ -18,9 +18,9 @@ module.exports = async (options = {}) => {
         Object.keys(processes).some((name) =>
           x.cmd.includes(processes[name])
         ) &&
-        x.cmd.includes('-childID') &&
+        x.cmd.includes("-childID") &&
         x.memory > kill_limit
     )
-    .map((x) => x.pid)
-  return fkill(pids, { force: true })
-}
+    .map((x) => x.pid);
+  return fkill(pids, { force: true });
+};
